773ba2c365d02e3edc26c293a9ff8c764fd84984
ChernenkoAndriy <andre.chrn@gmail.com>
2026-01-05 21:27:16 +0200

    Init

---
+++ sez/src/main/java/com/andruf/sez/SezApplication.java
@@ -0,0 +1,13 @@
+package com.andruf.sez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SezApplication.class, args);
+	}
+
+}
---
+++ sez/src/test/java/com/andruf/sez/SezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andruf.sez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
